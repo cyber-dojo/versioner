387d22315e6958a08cde7fe71b4f14aa32b58037
Jon Jagger <jon@jaggersoft.com>
2019-06-23 10:15:33 +0100

    refactoring; pull dot-env out into a constant

--- src/versioner.rb
+++ src/versioner.rb
@@ -1,4 +1,12 @@
 
+DOT_ENV = begin
+  src = IO.read('/app/.env')
+  lines = src.lines.reject do |line|
+    line.start_with?('#') || line.strip.empty?
+  end
+  lines.map { |line| line.split('=').map(&:strip) }.to_h
+end
+
 class Versioner
 
   def initialize(_external)
@@ -13,11 +21,7 @@ class Versioner
   end
 
   def dot_env
-    src = IO.read('/app/.env')
-    lines = src.lines.reject do |line|
-      line.start_with?('#') || line.strip.empty?
-    end
-    lines.map { |line| line.split('=').map(&:strip) }.to_h
+    DOT_ENV
   end
 
 end
