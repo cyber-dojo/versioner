87089b6d8d0c828be762ccd563ec9cb7b76c38d5
Jon Jagger <jon@jaggersoft.com>
2019-06-29 13:10:34 +0200

    added comments to echo_env_vars.rb script

--- src/echo_env_vars.rb
+++ src/echo_env_vars.rb
@@ -1,4 +1,14 @@
 
+# Use:
+#
+# $ docker run --rm cyberdojo/versioner:latest ruby /app/src/echo_env_vars.rb > /tmp/.env
+# $ set -a
+# $ . /tmp/.env
+# $ set +a
+#
+# This will set env-vars, such as
+# CYBER_DOJO_RUNNER_SHA=df2157263cad705c69a3fafe77ce4b78f54301ca
+# CYBER_DOJO_RUNNER_TAG=df21572
 
 DOT_ENV = begin
   src = IO.read('/app/.env')
@@ -37,3 +47,23 @@ DOT_ENV.each do |key,value|
     puts "#{tag_name(key)}=#{tag_value(value)}"
   end
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+# Notes: I would like to be able to use versioner's .env file as follows
+#
+# $ docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env' > /tmp/.env
+# $ set -a
+# $ . /tmp/.env
+# $ set +a
+#
+# this would give, eg
+# CYBER_DOJO_RUNNER_SHA=df2157263cad705c69a3fafe77ce4b78f54301ca
+#
+# and then in the docker-compose.yml file I would get the image tag from the sha.
+#
+# services:
+#   runner:
+#     image: cyberdojo/runner:${CYBER_DOJO_RUNNER_SHA:0:7}
+#
+# However, you cant use that :0:7 bashism inside a docker-compose yaml file :-(
+# So instead, there is this script, which generates the extra env vars.
