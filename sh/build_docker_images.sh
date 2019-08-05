499b9af1a7017b920bdc66a3222ecfc4a249dde4
JonJagger <jon@jaggersoft.com>
2019-08-05 19:49:11 +0100

    [RELEASE=0.1.20] patch-level updates to web/nginx now avatar images are not served by nginx

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -4,8 +4,13 @@ set -e
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
+readonly IMAGE=cyberdojo/versioner
+
 docker build \
   --build-arg RELEASE \
   --build-arg SHA \
-  --tag cyberdojo/versioner \
+  --tag ${IMAGE} \
   "${ROOT_DIR}"
+
+docker tag ${IMAGE}:latest ${IMAGE}:${SHA:0:7}
+docker run --rm ${IMAGE}:latest sh -c 'echo ${SHA}'
