da4207338c6217051384851ba33af2141e07126e
Jon Jagger <jon@jaggersoft.com>
2019-12-19 14:54:32 +0000

    run all tests with a single [docker run] command

--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
+# Script to run the tests from outside a web container
 ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 run_all_tests()
 {
-  for file in ${ROOT_DIR}/test/test_*.rb; do
-    filename=$(basename "${file}")
-    run_one_test "${filename}"
-  done
+  docker run \
+    --rm \
+    --tty \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume "${ROOT_DIR}/.env:/app/.env:ro" \
+    --volume "${ROOT_DIR}/test:/app/test:ro" \
+    cyberdojo/docker-base \
+      "/app/test/run_all.sh"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
@@ -17,6 +22,7 @@ run_one_test()
 {
   docker run \
     --rm \
+    --tty \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "${ROOT_DIR}/.env:/app/.env:ro" \
     --volume "${ROOT_DIR}/test:/app/test:ro" \
