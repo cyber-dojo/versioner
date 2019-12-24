8266498f2e1f4efd37ffcff7a63e720f50fc7d7c
Jon Jagger <jon@jaggersoft.com>
2019-12-24 11:39:28 +0000

    [RELEASE=0.1.71] Move towards custom/exercises/languages start-point env-vars following same format as other; fix broken test

--- test/image_name.rb
+++ test/image_name.rb
@@ -1,11 +1,15 @@
 
 module ImageName
 
-  def image_name(tag_env_name)
+  def image_name(tag_env_name) # eg CYBER_DOJO_AVATARS_TAG
     tag = dot_env(tag_env_name)
     parts = tag_env_name.split('_').map(&:downcase)
     name = parts[2..-2].join('-')
-    "cyberdojo/#{name}:#{tag}"
+    if name === 'languages-start-points'
+      "cyberdojo/#{name}-common:#{tag}"
+    else
+      "cyberdojo/#{name}:#{tag}"
+    end
   end
 
 end
