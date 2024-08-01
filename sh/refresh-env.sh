1214390866d71cc26abf9f7b7d4ed7aea33401ee
JonJagger <jon@kosli.com>
2024-08-01 15:07:51 +0100

    [RELEASE=0.1.390] Patch level updates

--- sh/refresh-env.sh
+++ sh/refresh-env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -Eeu
 
-readonly ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+repo_root() { git rev-parse --show-toplevel; }
 
-${ROOT_DIR}/sh/latest-env.sh    | tee ${ROOT_DIR}/app/.env
+$(repo_root)/sh/latest-env.sh    | tee $(repo_root)/app/.env
 if [ "${PIPESTATUS[0]}" != '0' ]; then
   exit 42
 fi
 
-${ROOT_DIR}/sh/latest-env-md.sh | tee ${ROOT_DIR}/app/.env.md
+$(repo_root)/sh/latest-env-md.sh | tee $(repo_root)/app/.env.md
 if [ "${PIPESTATUS[0]}" != '0' ]; then
   exit 42
 fi
